932f53b5c93ab52dc918c422bb2bf0f5610b6365
Adeyinka Giwa <adeyinka36@gmail.com>
2020-05-29 23:56:46 +0100

    no html errors

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
     gallery:{enabled:true},
-    image:{titleSrc:"desc"}
+    image:{titleSrc:"title"}
   
   
   });
